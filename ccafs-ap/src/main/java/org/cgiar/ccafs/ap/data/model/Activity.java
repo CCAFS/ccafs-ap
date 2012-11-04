c00cbdde69e83823a56f28354dbd21543bd78c7c
htobon <htobon@de749076-4b7e-b71b-d3c2-19c9df651b43>
2012-11-04 18:54:53 +0000

    example commited. First approx of the main structure.

---
+++ ccafs-ap/src/main/java/org/cgiar/ccafs/ap/data/model/Activity.java
@@ -0,0 +1,14 @@
+package org.cgiar.ccafs.ap.data.model;
+
+import java.util.Date;
+
+public class Activity {
+	private int id;
+	private String title;
+	private Date startDate;
+	private Date endDate;
+	private String description;
+	// private Milestone milestone;
+	// private ActivityLeader leader;
+	private boolean isGlobal;
+}
