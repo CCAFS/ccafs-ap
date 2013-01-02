40bd82de88fb61c5031e82c08cc851ab1a033291
htobon <htobon@de749076-4b7e-b71b-d3c2-19c9df651b43>
2013-01-02 13:40:50 +0000

    Adding remove functionality for each deliverable.

--- ccafs-ap/src/main/webapp/js/reporting/deliverablesReporting.js
+++ ccafs-ap/src/main/webapp/js/reporting/deliverablesReporting.js
@@ -40,40 +40,17 @@ $(document).ready(function() {
     $("#newDeliverablesGroup").append($newDeliverable);
     $("#newDeliverablesGroup").append("<hr />");
     renameDeliverables();
-    $newDeliverable.fadeIn("slow");          
-  
-    
-    //var num = $(".cloned").length; // how many "duplicatable" input fields we currently have
-   // var newNum = new Number(num + 1); // the numeric ID of the new input field being added
-
-    // create the new element via clone() 
-    //var newElem = $('#reportingDeliverable').clone(true);
-
-    // Add it's class and ID
-    //newElem.attr('id', 'reportingDeliverable' + newNum);
-    //newElem.attr('class', 'cloned');
-
-    //$(newElem).hide();
-
-    //if (num > 0) {
-      // insert the new element after the last "duplicatable" input field
-      //$('#reportingDeliverable' + num).after(newElem);
-    //} else {
-     // $('#newDeliverables legend').after(newElem);
-    //}
-
-    //$(newElem).show("slow");
-
+    $newDeliverable.fadeIn("slow");
   });
 
   $('.removeDeliverable').click(function(event) {
     event.preventDefault();
-    var num = $('.cloned').length; // how many "duplicatable" input fields we currently have
-
-    $(event.target).parent().parent().hide("slow", function() {
-      $(this).remove(); // remove the element
-    });
-
+    // Getting the id.
+    var removeId = $(event.target).attr("id").split("-")[1];
+    $("#deliverable-"+removeId).fadeOut("slow");
+    $("#deliverable-"+removeId).next("hr").remove();
+    $("#deliverable-"+removeId).remove();
+    renameDeliverables();
   });
 
 });
