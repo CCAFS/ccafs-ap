b15576d586a6d7e90def743a3a0aa0bef884d822
carvajal.hernandavid <carvajal.hernandavid@de749076-4b7e-b71b-d3c2-19c9df651b43>
2013-12-09 19:50:36 +0000

    Deleting a bad placed folder in Tags

--- tags/ccafs-ap_v1.1.2/trunk/ccafs-ap/src/main/webapp/js/reporting/partnerSaveReporting.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  if($("[name='activityID']").val() == 0) {
-    var message = $("#message\\.success").val();
-    alert(message);
-    window.close();
-  };
-});
--- tags/ccafs-ap_v1.1.2/trunk/ccafs-ap/src/main/webapp/js/reporting/tlOutputsReporting.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() { 
-
-  // character counter to description text areas.
-  $("#outputsGroup").find("[id$='description']").each(function() {
-    applyCharCounter($(this), 30000);
-  });
-  
-});
