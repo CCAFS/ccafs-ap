2565600de479143969461d98cc35c9b6371029fe
carvajal.hernandavid <carvajal.hernandavid@de749076-4b7e-b71b-d3c2-19c9df651b43>
2013-12-09 15:28:15 +0000

    There were deleted two lines that were printed to console.

--- branches/ccafs-ap_v1.2_dev/src/main/java/org/cgiar/ccafs/ap/action/planning/MainInformationPlanningAction.java
+++ branches/ccafs-ap_v1.2_dev/src/main/java/org/cgiar/ccafs/ap/action/planning/MainInformationPlanningAction.java
@@ -130,7 +130,6 @@ public class MainInformationPlanningAction extends BaseAction {
 
   @Override
   public void prepare() throws Exception {
-    System.out.println("--------------- PREPARE --------------------------");
     super.prepare();
 
     validationMessage = new StringBuilder();
@@ -173,7 +172,6 @@ public class MainInformationPlanningAction extends BaseAction {
 
   @Override
   public String save() {
-    System.out.println("--------------- SAVE --------------------------");
     boolean success = true;
     String finalMessage;
 
