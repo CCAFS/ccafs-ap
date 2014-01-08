4fe869546ded7c4a2b01aa3df84509953e5f9048
carvajal.hernandavid <carvajal.hernandavid@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-01-08 13:55:14 +0000

    Deleting the branch used to develop the version 1.2

--- branches/ccafs-ap_v1.2_dev/src/main/webapp/js/summaries/caseStudiesSummaries.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  
-  $("#caseStudies_countriesSelected").attr("data-placeholder", $("#countryListDefault").val());
-  $("#caseStudies_countriesSelected").chosen({search_contains:true});
-  $("#caseStudies_leadersSelected").chosen({search_contains:true});
-  $("#caseStudies_yearSelected").chosen();
-});
