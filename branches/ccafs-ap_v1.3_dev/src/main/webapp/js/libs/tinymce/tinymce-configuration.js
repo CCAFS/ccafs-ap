ccefb4eb448b2b0074d294c0d799d5f9c2feba60
carvajal.hernandavid <carvajal.hernandavid@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-04-30 14:32:36 +0000

    Added html editor to text boxes, not working properly yet.

---
+++ branches/ccafs-ap_v1.3_dev/src/main/webapp/js/libs/tinymce/tinymce-configuration.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  tinymce.init({
+    selector: "textarea",
+    toolbar: "bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent"
+  });
+  
+});
