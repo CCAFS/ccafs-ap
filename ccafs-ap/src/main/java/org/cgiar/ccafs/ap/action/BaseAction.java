135a8c9a1740d22ba5b088513ddfe0ddf2b6c9c3
carvajal.hernandavid <carvajal.hernandavid@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-06-17 20:17:14 +0000

    Applying the GPL license to all files inside the Action package

--- ccafs-ap/src/main/java/org/cgiar/ccafs/ap/action/BaseAction.java
+++ ccafs-ap/src/main/java/org/cgiar/ccafs/ap/action/BaseAction.java
@@ -1,3 +1,17 @@
+/*
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cgiar.ccafs.ap.action;
 
 import org.cgiar.ccafs.ap.config.APConfig;
@@ -175,6 +189,10 @@ public class BaseAction extends ActionSupport implements Preparable, SessionAwar
     return config.isSummariesActive();
   }
 
+  public String next() {
+    return NEXT;
+  }
+
   @Override
   public void prepare() throws Exception {
     // So far, do nothing here!
@@ -183,10 +201,6 @@ public class BaseAction extends ActionSupport implements Preparable, SessionAwar
   /* Override this method depending of the save action. */
   public String save() {
     return SUCCESS;
-  }
-
-  public String next() {
-    return NEXT;
   }
 
 
@@ -198,13 +212,13 @@ public class BaseAction extends ActionSupport implements Preparable, SessionAwar
     this.delete = delete;
   }
 
-  public void setSave(boolean save) {
-    this.save = true;
+  public void setNext(boolean next) {
+    this.next = true;
   }
 
 
-  public void setNext(boolean next) {
-    this.next = true;
+  public void setSave(boolean save) {
+    this.save = true;
   }
 
   @Override
