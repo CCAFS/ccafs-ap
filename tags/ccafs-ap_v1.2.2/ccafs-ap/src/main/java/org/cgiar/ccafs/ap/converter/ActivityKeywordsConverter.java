c00c9029f79d484b15ac253d3df47a817c222c6d
carvajal.hernandavid <carvajal.hernandavid@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-06-17 20:19:28 +0000

    It was applied the GPL licences in the config and converter packages

--- tags/ccafs-ap_v1.2.2/ccafs-ap/src/main/java/org/cgiar/ccafs/ap/converter/ActivityKeywordsConverter.java
+++ tags/ccafs-ap_v1.2.2/ccafs-ap/src/main/java/org/cgiar/ccafs/ap/converter/ActivityKeywordsConverter.java
@@ -1,3 +1,17 @@
+/*
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cgiar.ccafs.ap.converter;
 
 import org.cgiar.ccafs.ap.data.manager.KeywordManager;
@@ -16,6 +30,11 @@ import org.apache.struts2.util.StrutsTypeConverter;
 public class ActivityKeywordsConverter extends StrutsTypeConverter {
 
   private KeywordManager keywordManager;
+
+  @Inject
+  public ActivityKeywordsConverter(KeywordManager keywordManager) {
+    this.keywordManager = keywordManager;
+  }
 
   @Override
   public Object convertFromString(Map context, String[] values, Class toClass) {
@@ -43,11 +62,6 @@ public class ActivityKeywordsConverter extends StrutsTypeConverter {
       temp.add(ak.getId() + "");
     }
     return temp.toString();
-  }
-
-  @Inject
-  public ActivityKeywordsConverter(KeywordManager keywordManager) {
-    this.keywordManager = keywordManager;
   }
 
 }
