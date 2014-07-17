dff3b76ce17a14d51d5a2739d1f4d857ff249fcb
neojavan@gmail.com <neojavan@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-07-17 13:50:53 +0000

    Adding Methods for Insertion of Project and for consults on Project list

--- trunk/ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/action/preplanning/ProjectsListAction.java
+++ trunk/ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/action/preplanning/ProjectsListAction.java
@@ -4,14 +4,12 @@ import org.cgiar.ccafs.ap.action.BaseAction;
 import org.cgiar.ccafs.ap.config.APConfig;
 import org.cgiar.ccafs.ap.data.manager.ProjectManager;
 import org.cgiar.ccafs.ap.data.model.Employee;
-import org.cgiar.ccafs.ap.data.model.Leader;
 import org.cgiar.ccafs.ap.data.model.Project;
 import org.cgiar.ccafs.ap.data.model.User;
 
 import java.util.List;
 
 import com.google.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +25,7 @@ public class ProjectsListAction extends BaseAction {
   private static Logger LOG = LoggerFactory.getLogger(ProjectsListAction.class);
 
   // Model
-  List<Project> projects;
+  private List<Project> projects;
 
   @Inject
   public ProjectsListAction(APConfig config, ProjectManager projectManager) {
@@ -42,21 +40,21 @@ public class ProjectsListAction extends BaseAction {
 
   @Override
   public void prepare() throws Exception {
-	super.prepare();
-	
-	// Depending on the user that is logged-in, the list of projects will be displayed.
-	User fakeUser = new User();
-	fakeUser.setId(100);
-	fakeUser.setEmail("user@email.org");
-	fakeUser.setRole("RPL");
+    super.prepare();
+
+    // Depending on the user that is logged-in, the list of projects will be displayed.
+    User fakeUser = new User();
+    fakeUser.setId(100);
+    fakeUser.setEmail("user@email.org");
+    fakeUser.setRole("RPL");
     Employee projectLeader = new Employee(1, new User());
-    
-    
+
+
     // Getting project list.
     projects = projectManager.getAllProjects();
-    //projects = projectManager.getProject(projectLeader);
-    
+    // projects = projectManager.getProject(projectLeader);
+
     System.out.println(projects);
-    
+
   }
 }
