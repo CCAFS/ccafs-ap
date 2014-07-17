332a9505d5997648e854dddeeebd5b7ba7c37695
htobon <htobon@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-07-17 15:46:19 +0000

    Adding Institution and InstitutionType data models.

--- ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/data/model/InstitutionType.java
+++ ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/data/model/InstitutionType.java
@@ -1,69 +1,75 @@
 /*****************************************************************
  * This file is part of CCAFS Planning and Reporting Platform.
- * 
  * CCAFS P&R is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * at your option) any later version.
- * 
  * CCAFS P&R is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
  * You should have received a copy of the GNU General Public License
- * along with CCAFS P&R.  If not, see <http://www.gnu.org/licenses/>.
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************/
 package org.cgiar.ccafs.ap.data.model;
 
-import java.util.ArrayList;
-
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-
-public class Project {
-
-  public int id;
-  public String title;
-  private ArrayList<IPProgram> types;
-
-  public Project() {
-
+/**
+ * This class represents the Type of an Institution:
+ * CRP, Donnor, GO, NARES, etc.
+ * 
+ * @author Héctor Tobón
+ *
+ */
+public class InstitutionType {
+  private int id;
+  private String name;
+  private String acronym;
+  
+  public InstitutionType() {
+    super();    
   }
-
-  public Project(int id, String title) {
+  
+  public InstitutionType(int id, String name, String acronym) {
+    super();
     this.id = id;
-    this.title = title;
+    this.name = name;
+    this.acronym = acronym;
   }
-
-
+  
   public int getId() {
     return id;
   }
 
-  public String getTitle() {
-    return title;
-  }
-
-  public ArrayList<IPProgram> getTypes() {
-    return types;
-  }
-
+  
   public void setId(int id) {
     this.id = id;
   }
 
-  public void setTitle(String title) {
-    this.title = title;
+  
+  public String getName() {
+    return name;
   }
 
-
-  public void setTypes(ArrayList<IPProgram> types) {
-    this.types = types;
+  
+  public void setName(String name) {
+    this.name = name;
   }
 
+  
+  public String getAcronym() {
+    return acronym;
+  }
+
+  
+  public void setAcronym(String acronym) {
+    this.acronym = acronym;
+  }
+  
   @Override
   public String toString() {
     return ToStringBuilder.reflectionToString(this);
   }
+  
 }
