65b2d68b3abbb59caf52075cca806e1bc69fdfaf
neojavan@gmail.com <neojavan@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-07-25 18:43:32 +0000

    Adding the Region to getAllInstitutionByType on MySQL and Location Manager

--- trunk/ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLLocationDAO.java
+++ trunk/ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLLocationDAO.java
@@ -82,7 +82,7 @@ public class MySQLLocationDAO implements LocationDAO {
       LOG.error("Exception arised getting the Country for the user {}.", locationID, e);
     }
 
-    LOG.debug("-- getCountry() > Calling method executeQuery to get the results");
+    LOG.debug("-- getLocation() > Calling method executeQuery to get the results");
     return locationData;
   }
 
@@ -91,15 +91,43 @@ public class MySQLLocationDAO implements LocationDAO {
     LOG.debug(">> getLocationsByType( )");
 
     StringBuilder query = new StringBuilder();
-    query.append("SELECT le.id, le.name, le.code, le.element_type_id ");
-    query.append("FROM loc_elements le ");
-    query.append("INNER JOIN loc_element_types let ON let.id=le.element_type_id  ");
-    query.append("WHERE let.id =  ");
+    query.append("SELECT leo.id, leo.name, leo.code, leo.element_type_id, leo.parent_id as region_id, ");
+    query.append("let.name as region_name, let.code as region_code, let.element_type_id as region_element_type ");
+    query.append("FROM loc_elements leo ");
+    query.append("LEFT JOIN loc_elements let ON leo.parent_id = let.id   ");
+    query.append("INNER JOIN loc_element_types letd ON letd.id = leo.element_type_id  ");
+    query.append("WHERE letd.id =  ");
     query.append(typeID);
-    query.append(" ORDER BY le.name ");
+    query.append(" ORDER BY leo.name ");
 
-    LOG.debug("-- getLocationsByType() > Calling method executeQuery to get the results");
-    return getData(query.toString());
+    List<Map<String, String>> locationsList = new ArrayList<>();
+
+    try (Connection con = databaseManager.getConnection()) {
+      ResultSet rs = databaseManager.makeQuery(query.toString(), con);
+      while (rs.next()) {
+        Map<String, String> locationData = new HashMap<String, String>();
+        locationData.put("id", rs.getString("id"));
+        locationData.put("name", rs.getString("name"));
+        locationData.put("code", rs.getString("code"));
+        // Region
+        locationData.put("region_id", rs.getString("region_id"));
+        locationData.put("region_name", rs.getString("region_name"));
+        locationData.put("region_code", rs.getString("region_code"));
+
+
+        locationsList.add(locationData);
+      }
+      rs.close();
+    } catch (SQLException e) {
+      String exceptionMessage = "-- executeQuery() > Exception raised trying ";
+      exceptionMessage += "to execute the following query " + query;
+      LOG.error(exceptionMessage, e);
+      return null;
+    }
+
+    LOG.debug("<< executeQuery():getLocationsByType.size={}", locationsList.size());
+    return locationsList;
+
   }
 
 }
