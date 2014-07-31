0a3adabb2ae5c67de24bad5bd042b0035b9176e9
htobon <htobon@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-07-31 12:58:09 +0000

    Creating Project Outcome object.

---
+++ ccafspr_ip/src/main/java/org/cgiar/ccafs/ap/data/model/ProjectOutcome.java
@@ -0,0 +1,74 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.ap.data.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+
+/**
+ * This class represents a Project Outcome.
+ * Is mostly used for the Planning section.
+ *
+ * @author Héctor Fabio Tobón R.
+ */
+public class ProjectOutcome {
+
+  private int id;
+  private int year;
+  private String statement;
+  private String stories;
+
+  public ProjectOutcome(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getStatement() {
+    return statement;
+  }
+
+  public String getStories() {
+    return stories;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setStatement(String statement) {
+    this.statement = statement;
+  }
+
+  public void setStories(String stories) {
+    this.stories = stories;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+
+
+}
