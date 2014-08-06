48ed48f4b242dc68e0ba0861bfe9b76b5ab9af9c
sebas932@gmail.com <sebas932@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-08-06 20:23:32 +0000

    Making validation for remove midOutcomes elements for FPLs

--- trunk/ccafspr_ip/src/main/webapp/js/preplanning/midOutcomesPreplanning.js
+++ trunk/ccafspr_ip/src/main/webapp/js/preplanning/midOutcomesPreplanning.js
@@ -3,140 +3,166 @@ var lWordsElemetDesc = 10;
 var lWordsIndicatorDesc = 50;
 
 $(document).ready(function(){
-	init();
+  init();
 });
 
-function init(){ 
-	attachEvents();
-	if(!$("div#MidOutcomeBlocks .midOutcome").length){
-	  $("div#addMidOutcomeBlock").trigger( "click" );
-	} else{
-      applyWordCounter($("form .midOutcome > .textArea textarea"), lWordsElemetDesc); 
-      applyWordCounter($("form .indicatorsBlock textarea"), lWordsIndicatorDesc);
-	}
-	setMidOutcomesIndexes();
+function init(){
+  attachEvents();
+  if (!$("div#MidOutcomeBlocks .midOutcome").length) {
+    $("div#addMidOutcomeBlock").trigger("click");
+  } else {
+    applyWordCounter($("form .midOutcome > .textArea textarea"), lWordsElemetDesc);
+    applyWordCounter($("form .indicatorsBlock textarea"), lWordsIndicatorDesc);
+  }
+  setMidOutcomesIndexes();
 }
 
 function attachEvents(){
-  //Mid Outcomes
+  // Mid Outcomes
   $("div#addMidOutcomeBlock").click(addMidOutcomeEvent);
-  $(".removeMidOutcomeBlock #removeMidOutcome").click(removeMidOutcomeEvent);	
-  //Contributes
+  $(".removeMidOutcomeBlock #removeMidOutcome").click(removeMidOutcomeEvent);
+  // Contributes
   $(".addContributeBlock input.addButton").click(addContributeEvent);
-  $(".removeContribute").click(removeContributeEvent);  
-  //Indicators
+  $(".removeContribute").click(removeContributeEvent);
+  // Indicators
   $(".indicatorsBlock input.addButton").click(addIndicatorEvent);
   $(".removeMidOutcomeIndicator").click(removeIndicatorEvent);
 }
 
-
-//----------------- Mid Outcomes Events ----------------------//
+// ----------------- Mid Outcomes Events ----------------------//
 function addMidOutcomeEvent(event){
-  event.preventDefault(); 
-  var $newElement = $("#midOutcomeTemplate").clone(true).removeAttr("id");  
+  event.preventDefault();
+  var $newElement = $("#midOutcomeTemplate").clone(true).removeAttr("id");
   $("div#MidOutcomeBlocks").append($newElement);
-  applyWordCounter($newElement.find("> .textArea textarea"), lWordsElemetDesc);  
+  applyWordCounter($newElement.find("> .textArea textarea"), lWordsElemetDesc);
   applyWordCounter($newElement.find(".indicatorsBlock textarea"), lWordsIndicatorDesc);
   $newElement.show("slow");
   setMidOutcomesIndexes();
 }
 
 function removeMidOutcomeEvent(event){
-  event.preventDefault(); 
-  var $ElementDiv = $(event.target).parent().parent(); 
-  $ElementDiv.hide("slow", function() { 
-	$(this).remove(); 
-	setMidOutcomesIndexes();
-  });  
+  event.preventDefault();
+  var $ElementDiv = $(event.target).parent().parent();
+  var elementID = $ElementDiv.find("[id^='midOutcomeId']").val();
+  var source = "../json/ipElementsByParent.do?elementID=" + elementID;
+  $.getJSON(source, function(){
+  }).done(function(data){
+    if (data.IPElementsList.length == 0) {
+      $ElementDiv.hide("slow", function(){
+        $(this).remove();
+        setMidOutcomesIndexes();
+      });
+    } else {
+      $("#removeDialog").find(".elements").html(data.IPElementsList.length);
+      $("#removeDialog").dialog({
+        modal : true,
+        buttons : {
+          "Remove" : function(){
+            $ElementDiv.hide("slow", function(){
+              $(this).remove();
+              setMidOutcomesIndexes();
+            });
+            $(this).dialog("close");
+          },
+          "Cancel" : function(){
+            $(this).dialog("close");
+          }
+        }
+      });
+    }
+  }).fail(function(){
+    console.log("error");
+  });
+  
 }
 
-
 function setMidOutcomesIndexes(){
-	//console.log($("div#MidOutcomeBlocks .midOutcome"));
-  $("div#MidOutcomeBlocks .midOutcome").each(function(index, element){
-     
-      var elementName = "midOutcomes[" + index + "]."; 
-      $(element).attr("id","midOutcome-"+index);
-      // Hidden inputs
-      $(element).find("[id^='midOutcomeId']").attr("name", elementName + "id");
-      $(element).find("[id^='midOutcomeProgramID']").attr("name", elementName + "program.id");
-      $(element).find("[id^='midOutcomeTypeID']").attr("name", elementName + "type.id");
-      // Visible inputs
-      $(element).find("[id$='elementIndex']").html(index+1);
-      $(element).find("textarea[name$='description']").attr("name", elementName + "description").attr("placeholder", "Add outcome #"+ (index+1) );
-      //console.log('setMidOutcomesIndexes -->'+index+ ' -->'+$(element).attr('id') ); 
-      setContributesIndexes(index);
-      setIndicatorsIndexes(index);
-     
+  // console.log($("div#MidOutcomeBlocks .midOutcome"));
+  $("div#MidOutcomeBlocks .midOutcome").each(function(index,element){
+    
+    var elementName = "midOutcomes[" + index + "].";
+    $(element).attr("id", "midOutcome-" + index);
+    // Hidden inputs
+    $(element).find("[id^='midOutcomeId']").attr("name", elementName + "id");
+    $(element).find("[id^='midOutcomeProgramID']").attr("name", elementName + "program.id");
+    $(element).find("[id^='midOutcomeTypeID']").attr("name", elementName + "type.id");
+    // Visible inputs
+    $(element).find("[id$='elementIndex']").html(index + 1);
+    $(element).find("textarea[name$='description']").attr("name", elementName + "description").attr("placeholder", "Add outcome #" + (index + 1));
+    // console.log('setMidOutcomesIndexes -->'+index+ ' -->'+$(element).attr('id') );
+    setContributesIndexes(index);
+    setIndicatorsIndexes(index);
+    
   });
 }
 
-//----------------- Contribute Events ----------------------//
+// ----------------- Contribute Events ----------------------//
 function addContributeEvent(event){
-	event.preventDefault();  
-	var $addButton = $(event.target).parent(); 
-	var $selectElemet = $(event.target).siblings().find("select");
-	var $optionSelected = $selectElemet.find('option:selected');
-	
-	if ($selectElemet.find('option').length != 0){
-		var $newElementClone = $("#contributeTemplate").clone(true).removeAttr("id");
-		var grandParentId = $addButton.parent().parent().attr("id").split("-")[1];
-		$newElementClone.find("[value]").attr("value", $optionSelected.attr("value"));
-		$newElementClone.find('p').html($optionSelected.html());
-		$addButton.before($newElementClone);
-		$newElementClone.show("slow");  
-		$optionSelected.remove(); 
-		setContributesIndexes(grandParentId);
-	}
+  event.preventDefault();
+  var $addButton = $(event.target).parent();
+  var $selectElemet = $(event.target).siblings().find("select");
+  var $optionSelected = $selectElemet.find('option:selected');
+  
+  if ($selectElemet.find('option').length != 0) {
+    var $newElementClone = $("#contributeTemplate").clone(true).removeAttr("id");
+    var grandParentId = $addButton.parent().parent().attr("id").split("-")[1];
+    $newElementClone.find("[value]").attr("value", $optionSelected.attr("value"));
+    $newElementClone.find('p').html($optionSelected.html());
+    $addButton.before($newElementClone);
+    $newElementClone.show("slow");
+    $optionSelected.remove();
+    setContributesIndexes(grandParentId);
+  }
 }
 
 function removeContributeEvent(event){
-  event.preventDefault(); 
+  event.preventDefault();
   var $elementDiv = $(event.target).parent().parent();
   var $parentDiv = $elementDiv.parent().parent();
-  $elementDiv.hide("slow", function() {
-	var i = $parentDiv.attr("id").split("-")[1];  
-	$parentDiv.find("select").append('<option value="'+$elementDiv.find("input").attr("value")+'">'+$elementDiv.find("p").html()+'</option>');  
-    $(this).remove();  
+  console.log("remove contribute ");
+  $elementDiv.hide("slow", function(){
+    var i = $parentDiv.attr("id").split("-")[1];
+    $parentDiv.find("select").append('<option value="' + $elementDiv.find("input").attr("value") + '">' + $elementDiv.find("p").html() + '</option>');
+    $(this).remove();
     setContributesIndexes(i);
-  }); 
-}
-
-function setContributesIndexes(i){
-  $("#midOutcome-"+i+" div.contributions").each(function(index, element){
-      var elementName = "midOutcomes["+i+"].contributesTo";
-      $(element).find("[id^='contributeId']").attr("name", elementName).attr("value", outcome.id);
   });
 }
 
-// -----------------   Indicators Events  ----------------------//
+function setContributesIndexes(i){
+  $("#midOutcome-" + i + " div.contributions").each(function(index,element){
+    var elementName = "midOutcomes[" + i + "].contributesTo";
+    $(element).find("[id^='contributeId']").attr("name", elementName).attr("value", outcome.id);
+  });
+}
+
+// ----------------- Indicators Events ----------------------//
 function addIndicatorEvent(event){
   var grandParentId = $(event.target).parent().parent().parent().attr("id").split("-")[1];
-  //console.log(grandParentId);
-  var $newIndicator = $("#midOutcomeTemplate div.indicator").clone(true).css("display","none"); 
+  // console.log(grandParentId);
+  var $newIndicator = $("#midOutcomeTemplate div.indicator").clone(true).css("display", "none");
   $(event.target).parent().before($newIndicator);
   applyWordCounter($newIndicator.find("textarea"), lWordsIndicatorDesc);
   $newIndicator.show("slow");
   setIndicatorsIndexes(grandParentId);
 }
 
-function removeIndicatorEvent(event){ 
+function removeIndicatorEvent(event){
   event.preventDefault();
   var grandParentId = $(event.target).parent().parent().parent().parent().attr("id").split("-")[1];
   var pressedLink = event.target;
   var $indicatorDiv = $(event.target).parent().parent();
   var $parentDiv = $indicatorDiv.parent().parent();
-  $indicatorDiv.hide("slow", function() {
+  $indicatorDiv.hide("slow", function(){
     $(this).remove();
     setIndicatorsIndexes(grandParentId);
-  }); 
+  });
 }
 
 function setIndicatorsIndexes(i){
-  $("#midOutcome-"+i+" div.indicator").each(function(index, indicator){
-      var elementName = "midOutcomes["+i+"].indicators[" + index + "].";  
-      $(indicator).find("[name$='id']").attr("name", elementName + "id");
-      $(indicator).find("[id$='description']").attr("name", elementName + "description").attr("placeholder", "Add indicator #"+ (index+1) ); 
-      $(indicator).find("[id$='target']").attr("name", elementName + "target");
+  $("#midOutcome-" + i + " div.indicator").each(function(index,indicator){
+    var elementName = "midOutcomes[" + i + "].indicators[" + index + "].";
+    $(indicator).find("[name$='id']").attr("name", elementName + "id");
+    $(indicator).find("[id$='description']").attr("name", elementName + "description").attr("placeholder", "Add indicator #" + (index + 1));
+    $(indicator).find("[id$='target']").attr("name", elementName + "target");
   });
 }
