42f309882935352def8beb3a0dc6f6b23d6d8f81
neojavan@gmail.com <neojavan@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-08-28 22:56:27 +0000

    Adding modal property to the image P&R proccess workflow

--- ccafspr_ip/src/main/webapp/js/home/dashboard.js
+++ ccafspr_ip/src/main/webapp/js/home/dashboard.js
@@ -8,4 +8,19 @@ $(document).ready(function(){
   $("#activities-table").dataTable({
     "iDisplayLength" : 5
   });
+  
 });
+
+function workflowModal(){
+  $( "#showPandRWorkflowDialog" ).dialog({
+    modal: true,
+    width: 700,
+    height: 770,
+    buttons: {
+      Ok: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  });
+  return false;
+}
