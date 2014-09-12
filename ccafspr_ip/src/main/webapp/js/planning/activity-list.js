319c274f737b9196476fb9005f6b905a34bf02b7
sebas932@gmail.com <sebas932@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2014-09-12 20:52:42 +0000

    Started with revamp of Project Budget Section.

--- ccafspr_ip/src/main/webapp/js/planning/activity-list.js
+++ ccafspr_ip/src/main/webapp/js/planning/activity-list.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function(){
   
   $('table.activitiesList').dataTable({
     "bPaginate" : true, // This option enable the table pagination
     "bLengthChange" : false, // This option disables the select table size option
     "bFilter" : true, // This option enable the search
     "bSort" : true, // this option enable the sort of contents by columns
-    "bAutoWidth" : true, // This option enables the auto adjust columns width
+    "bAutoWidth" : false, // This option enables the auto adjust columns width
     "iDisplayLength" : 15, // Number of rows to show on the table
-    "fnDrawCallback": function(){
+    "fnDrawCallback" : function(){
       // This function locates the add activity button at left to the filter box
       var table = $(this).parent().find("table");
-      if( $(table).attr("id") == "currentActivities"){
+      if ($(table).attr("id") == "currentActivities") {
         $("#currentActivities_filter").prepend($("#addActivity"));
       }
     }
   });
   
   $("#submitForm").on("submit", function(evt){
-    if(confirm($("#beforeSubmitMessage").val())){
+    if (confirm($("#beforeSubmitMessage").val())) {
       return true;
-    }else{
+    } else {
       evt.preventDefault();
       return false;
     }
