248e3525d05983acdc17670a62ce6415b3dda16e
sebas932@gmail.com <sebas932@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2015-01-13 18:47:11 +0000

    Improved deliverable data sharing section  #4

--- trunk/ccafs-ap/src/main/webapp/js/reporting/deliverables/deliverablesReportingDataSharing.js
+++ trunk/ccafs-ap/src/main/webapp/js/reporting/deliverables/deliverablesReportingDataSharing.js
@@ -1,8 +1,30 @@
+Dropzone.autoDiscover = false;
 $(document).ready(function(){
-  console.log("Deliverables Data Sharing");
   
-  $("div#dragAndDrop").dropzone({
-    url : "/file/post"
-  });
+  attachEvents();
+  // Create a dropzone to attach files
+  addDropzone();
+  // Check option selected
+  checkOption();
+  
+  function attachEvents(){
+    $("input[name^='dataSharingOptions']").on("click", checkOption);
+  }
+  
+  function checkOption(){
+    if ($("input#option-3").is(":checked")) {
+      $("div#fileURL").hide();
+      $("div#dragAndDrop").show();
+    } else {
+      $("div#dragAndDrop").hide();
+      $("div#fileURL").show();
+    }
+  }
+  
+  function addDropzone(){
+    $("div#dragAndDrop").dropzone({
+      url : "/file/post"
+    });
+  }
   
 });
