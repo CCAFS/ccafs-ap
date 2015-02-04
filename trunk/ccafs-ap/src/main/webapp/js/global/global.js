ae69b50f8727b1bacc23e864c1e85acea154e080
sebas932@gmail.com <sebas932@gmail.com@de749076-4b7e-b71b-d3c2-19c9df651b43>
2015-02-04 19:06:07 +0000

    Improved function of deliverable data sharing section #3

--- trunk/ccafs-ap/src/main/webapp/js/global/global.js
+++ trunk/ccafs-ap/src/main/webapp/js/global/global.js
@@ -1,59 +1,69 @@
 // Global javascript must be here.
-$(document).ready(function() {
+$(document).ready(function(){
   showNotificationMessages();
   
-  function showNotificationMessages() {
-    $('#generalMessages #messages').children("li").each(function(index) {
+  function showNotificationMessages(){
+    $('#generalMessages #messages').children("li").each(function(index){
       // Validate if the notification is a warning checking if the text contains --warn--
       var message = $(this).text();
-      if(message.lastIndexOf("--warn--", 0) === 0){
+      if (message.lastIndexOf("--warn--", 0) === 0) {
         message = message.replace("--warn--", " ");
         $('#generalMessages').noty({
-          type: 'warning', 
-          text: message
+          type : 'warning',
+          text : message
         });
         
-      }else{
+      } else {
         $('#generalMessages').noty({
-          type: $(this).attr("class"), 
-          text: $(this).text()
+          type : $(this).attr("class"),
+          text : $(this).text()
         });
       }
     });
   }
   
-  /* Tooltips with JQuery UI*/  
+  /* Tooltips with JQuery UI */
   $(this).tooltip({
-    track: true
+    track : true
   });
   
 });
 
 /* Add a char counter to a specific text area */
-function applyCharCounter($textArea, charCount) {
-  $textArea.parent().append("<p class='charCount'>(<span>"+charCount+"</span> characters remaining)</p>");
-  $textArea.next(".charCount").find("span").text(charCount-$textArea.val().length);
-  $textArea.on("keyup", function(event) {
-    if($(event.target).val().length > charCount) {
-      $(event.target).val($(event.target).val().substr(0,charCount));
+function applyCharCounter($textArea,charCount){
+  $textArea.parent().append("<p class='charCount'>(<span>" + charCount + "</span> characters remaining)</p>");
+  $textArea.next(".charCount").find("span").text(charCount - $textArea.val().length);
+  $textArea.on("keyup", function(event){
+    if ($(event.target).val().length > charCount) {
+      $(event.target).val($(event.target).val().substr(0, charCount));
     }
-    $(event.target).next(".charCount").find("span").text(charCount-$(event.target).val().length);
+    $(event.target).next(".charCount").find("span").text(charCount - $(event.target).val().length);
   });
+  $textArea.trigger("keyup");
 }
 
 /* Add a word counter to a specific text area */
-function applyWordCounter($textArea, wordCount) {
-  $textArea.parent().append("<p class='charCount'>(<span>"+wordCount+"</span> words remaining)</p>");
-  $textArea.next(".charCount").find("span").text(wordCount-word_count($textArea));
-  $textArea.on("keyup", function(event) {
-    if(word_count($textArea) > wordCount) {
-      $(event.target).val($(event.target).val().slice(0,-2));
+function applyWordCounter($textArea,wordCount){
+  $textArea.parent().append("<p class='charCount'>(<span>" + wordCount + "</span> words remaining)</p>");
+  $textArea.next(".charCount").find("span").text(wordCount - word_count($textArea));
+  $textArea.on("keyup", function(event){
+    var $charCount = $(event.target).next(".charCount");
+    if (word_count($(event.target)) > wordCount) {
+      $(event.target).val($(event.target).val().slice(0, -2));
+      $(event.target).addClass('fieldError');
+      $charCount.addClass('fieldError');
+    } else {
+      $(event.target).removeClass('fieldError');
+      $charCount.removeClass('fieldError');
     }
-    $(event.target).next(".charCount").find("span").text(wordCount-word_count(event.target));
+    // Set count value
+    $charCount.find("span").text(wordCount - word_count(event.target));
+    
   });
+  $textArea.trigger("keyup");
 }
 
-function word_count(field) {
+function word_count(field){
   var matches = $(field).val().match(/\b/g);
   return number = (matches) ? matches.length / 2 : 0;
 }
