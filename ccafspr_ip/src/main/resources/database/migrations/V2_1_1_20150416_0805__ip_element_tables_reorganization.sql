bedbdc1dae3b5eff069a50c9a4ce82ca7e257e70
Hernan <carvajal.hernandavid@gmail.com>
2015-04-16 13:34:35 -0500

    Changed migration file to add an `ip_element_id` column from the `ip_indicators` table to remove the reference to the `ip_program_elements` table.

--- ccafspr_ip/src/main/resources/database/migrations/V2_1_1_20150416_0805__ip_element_tables_reorganization.sql
+++ ccafspr_ip/src/main/resources/database/migrations/V2_1_1_20150416_0805__ip_element_tables_reorganization.sql
@@ -10,11 +10,18 @@ DELIMITER $$
 CREATE PROCEDURE change_ip_elements_relations()
 BEGIN
 
-  -- Add the is_active column to the activities table, if it doesn't already exist
+  -- Add a reference to the ip_programs table in the ip_element table to now to which program belongs the element
   IF NOT EXISTS ((SELECT * FROM information_schema.columns WHERE table_schema=DATABASE() AND table_name='ip_elements' AND column_name='ip_program_id')) THEN
 		ALTER TABLE `ip_elements` ADD `ip_program_id` BIGINT NOT NULL ;		
-		UPDATE `ip_elements` ie SET  `ip_program_id` = ( SELECT program_id FROM  ip_program_elements ipe WHERE element_id = ie.id AND relation_type_id = 1 )
+		UPDATE `ip_elements` ie SET  `ip_program_id` = ( SELECT program_id FROM  ip_program_elements ipe WHERE element_id = ie.id AND relation_type_id = 1 );
 		ALTER TABLE ip_elements ADD CONSTRAINT FK_ip_elements_ip_programs_owner_id FOREIGN KEY (`ip_program_id`)  REFERENCES ip_programs(id);
+  END IF;
+  
+  -- Add a referece to which element belongs the indicator
+  IF NOT EXISTS ((SELECT * FROM information_schema.columns WHERE table_schema=DATABASE() AND table_name='ip_indicators' AND column_name='ip_element_id')) THEN
+    ALTER TABLE `ip_indicators` ADD `ip_element_id` BIGINT NOT NULL AFTER `target`;   
+    UPDATE `ip_indicators` ii SET  `ip_element_id` = ( SELECT element_id FROM  ip_program_elements ipe WHERE ipe.id = ii.program_element_id );
+    ALTER TABLE ip_indicators ADD CONSTRAINT FK_ip_indicators_ip_element_element_id FOREIGN KEY (`ip_element_id`)  REFERENCES ip_elements(id);
   END IF;
  
 END $$
