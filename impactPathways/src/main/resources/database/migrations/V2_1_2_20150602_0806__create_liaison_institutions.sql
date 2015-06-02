2319455a3428ce50bf3e52101467f1f521646751
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-06-02 08:27:09 -0500

    Added migration file to create table that will contain the liaison institutions.

---
+++ impactPathways/src/main/resources/database/migrations/V2_1_2_20150602_0806__create_liaison_institutions.sql
@@ -0,0 +1,43 @@
+-- -----------------------------------------------------------------------------
+-- Create table to store what insitutions can be management liaison
+-- -----------------------------------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `liaison_institutions` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `institution_id` BIGINT NOT NULL ,
+  `name` VARCHAR(255) NOT NULL ,
+  `acronym` VARCHAR(255) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `FK_liaison_institutions_institutions_idx` (`institution_id` ASC) ,
+  CONSTRAINT `FK_liaison_institutions_institutions` 
+    FOREIGN KEY (`institution_id` )
+    REFERENCES `institutions` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+DEFAULT CHARACTER SET = utf8;
+
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('697', 'Coordinating Unit', 'CU');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'Flagship One', 'FP1');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'Flagship Two', 'FP2');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('1053', 'Flagship Three', 'FP3');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'Flagship Four', 'FP4');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'East Africa Region', 'RP EA');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'Latin America Region', 'RP LAM');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'South Asia Region', 'RP SAs');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'South East Asia Region', 'RP SEA');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'West Africa Region', 'RP WA');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('52', 'Africa Rice Center', 'AfricaRice');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('49', 'Bioversity International', 'BI');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('46', 'Centro Internacional de Agricultura Tropical', 'CIAT');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('115', 'Center for International Forestry Research', 'CIFOR');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('50', 'International Maize and Wheat Improvement Center', 'CIMMYT');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('67', 'Centro Internacional de la Papa', 'CIP');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('51', 'International Center for Agricultural Research in the Dry Areas', 'ICARDA');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('88', 'World Agroforestry Centre', 'ICRAF');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('103', 'International Crops Research Institute for the Semi-Arid Tropics', 'ICRISAT');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('89', 'International Food Policy Research Institute', 'IFPRI');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('45', 'International Institute of Tropical Agriculture', 'IITA');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('66', 'International Livestock Research Institute', 'ILRI');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('5', 'International Rice Research Institute', 'IRRI');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('172', 'International Water Management Institute', 'IWMI');
+INSERT INTO `ccafspr_ip`.`liaison_institutions` (`institution_id`, `name`, `acronym`) VALUES ('99', 'WorldFish Center', 'WorldFish');
