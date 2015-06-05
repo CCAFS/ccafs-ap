9f94e742d66790260d07d2bb35ced50be264a560
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-06-05 13:37:34 -0500

    Added the liaison institutions to the model.

---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/LiaisonInstitutionDAO.java
@@ -0,0 +1,46 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.ap.data.dao;
+
+import org.cgiar.ccafs.ap.data.dao.mysql.MySQLLiaisonInstitutionDAO;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.inject.ImplementedBy;
+
+
+/**
+ * @author Hernán David Carvajal B. - CIAT/CCAFS
+ */
+
+@ImplementedBy(MySQLLiaisonInstitutionDAO.class)
+public interface LiaisonInstitutionDAO {
+
+  /**
+   * This method gets the liaison institution identified by the value received by parameter.
+   * 
+   * @param liaisionInstitutionID - Liaison institution identifier
+   * @return a Map with the information of the liaison institution.
+   */
+  public Map<String, String> getLiaisonInstitution(int liaisionInstitutionID);
+
+  /**
+   * This method return all the liaison institutions contained in the database.
+   * 
+   * @return a List of maps with the information.
+   */
+  public List<Map<String, String>> getLiaisonInstitutions();
+}
---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/manager/LiaisonInstitutionManager.java
@@ -0,0 +1,46 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.ap.data.manager;
+
+import org.cgiar.ccafs.ap.data.manager.impl.LiaisonInstitutionManagerImpl;
+import org.cgiar.ccafs.ap.data.model.LiaisonInstitution;
+
+import java.util.List;
+
+import com.google.inject.ImplementedBy;
+
+
+/**
+ * @author Hernán David Carvajal B. - CIAT/CCAFS
+ */
+
+@ImplementedBy(LiaisonInstitutionManagerImpl.class)
+public interface LiaisonInstitutionManager {
+
+  /**
+   * This method gets the liaison institution identified by the value received by parameter.
+   * 
+   * @param liaisionInstitutionID - Liaison institution identifier
+   * @return a liaison institution object.
+   */
+  public LiaisonInstitution getLiaisonInstitution(int liaisionInstitutionID);
+
+  /**
+   * This method return all the liaison institutions contained in the database.
+   * 
+   * @return a List of maps with the information.
+   */
+  public List<LiaisonInstitution> getLiaisonInstitutions();
+}
