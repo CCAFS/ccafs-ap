beefa829f5b5753fa7c09433c4cb1307903bdb71
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-06-18 10:18:25 -0500

    Added classes to get the log history details.

---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/HistoryDAO.java
@@ -0,0 +1,40 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.ap.data.dao;
+
+import org.cgiar.ccafs.ap.data.dao.mysql.MySQLHistoryDAO;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.inject.ImplementedBy;
+
+
+/**
+ * @author Hernán David Carvajal B. - CIAT/CCAFS
+ */
+
+@ImplementedBy(MySQLHistoryDAO.class)
+public interface HistoryDAO {
+
+  /**
+   * This method return the list of changes (only the user, date, action and justification) recorded in the history
+   * table corresponding to the table received by parameter.
+   * 
+   * @param tableName
+   * @return a list of maps with the information
+   */
+  public List<Map<String, String>> getHistoryList(String tableName, int record_id);
+}
