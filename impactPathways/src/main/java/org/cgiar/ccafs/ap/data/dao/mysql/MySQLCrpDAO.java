82e349d7bb52d6f3f2bc05591cb5dbcba564065d
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-06-22 14:07:10 -0500

    Added DAO classes for table CRps

---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLCrpDAO.java
@@ -0,0 +1,66 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.ap.data.dao.mysql;
+
+import org.cgiar.ccafs.ap.data.dao.CrpDAO;
+import org.cgiar.ccafs.utils.db.DAOManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author Hernán David Carvajal B. - CIAT/CCAFS
+ */
+
+public class MySQLCrpDAO implements CrpDAO {
+
+  private static Logger LOG = LoggerFactory.getLogger(MySQLCrpDAO.class);
+  private DAOManager daoManager;
+
+  public MySQLCrpDAO(DAOManager daoManager) {
+    this.daoManager = daoManager;
+  }
+
+  @Override
+  public List<Map<String, String>> getCRPsList() {
+    List<Map<String, String>> crps = new ArrayList<>();
+    String query = "SELECT * FROM crps WHERE is_active = TRUE";
+
+    try (Connection con = daoManager.getConnection()) {
+      ResultSet rs = daoManager.makeQuery(query, con);
+      while (rs.next()) {
+        Map<String, String> crp = new HashMap<>();
+        crp.put("id", rs.getString("id"));
+        crp.put("name", rs.getString("name"));
+        crp.put("acronym", rs.getString("acronym"));
+        crps.add(crp);
+      }
+    } catch (SQLException e) {
+      LOG.error("getCrpsList() > Exception raised trying to get the list of CRPs.", e);
+    }
+
+    return crps;
+  }
+
+}
