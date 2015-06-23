3ab1d03022403d58f7d1dcec05eed15a03a04b22
Jorge_Solis <jorgesolis1989@gmail.com>
2015-06-23 15:01:03 -0500

    Litte change for before file migrated_activity_locations_as_project_locations.
    The change was to skip the elimination of column activity_id.

--- impactPathways/src/main/resources/database/migrations/V2_1_2_20150623_1417__migrated_activity_locations_as_project_locations.sql
+++ impactPathways/src/main/resources/database/migrations/V2_1_2_20150623_1417__migrated_activity_locations_as_project_locations.sql
@@ -9,5 +9,9 @@ ALTER TABLE activity_locations ADD project_id  bigint(20)  NOT NULL AFTER id;
 UPDATE activity_locations actlo INNER JOIN activities act ON act.id = actlo.activity_id  
 SET actlo.project_id = act.project_id;
 
+
+-- Foreign Key between activities and activity_locations with project_id.
+ALTER TABLE activity_locations ADD CONSTRAINT  FK_activity_locations_activities_project_id FOREIGN KEY (project_id) REFERENCES activities(project_id);
+
 -- Rename table activity_locations to project_locations.
-ALTER TABLE activity_locations  RENAME TO project_locactions;
+ALTER TABLE activity_locations  RENAME TO project_locations;
