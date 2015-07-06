160ed58d21d11ea76d73b2a12bc528aa40508eda
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-07-06 13:31:34 -0500

    Improved the method to get the CCAFS outcome history

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLHistoryDAO.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLHistoryDAO.java
@@ -49,17 +49,34 @@ public class MySQLHistoryDAO implements HistoryDAO {
     String dbName = this.getDatabaseName();
 
     StringBuilder query = new StringBuilder();
-    query.append("SELECT u.id as 'user_id', u.first_name, u.last_name, u.email, t.action, ");
-    query.append("t.active_since, t.modification_justification ");
-    query.append("FROM ");
+    query.append("SELECT * FROM ( ");
+
+    query.append("  SELECT u.id as 'user_id', u.first_name, u.last_name, u.email, t.action, ");
+    query.append("  t.active_since, t.modification_justification ");
+    query.append("  FROM ");
     query.append(dbName);
     query.append("_history.ip_project_contributions t ");
-    query.append("INNER JOIN users u ON t.modified_by = u.id ");
-    query.append("WHERE project_id = ");
+    query.append("  INNER JOIN users u ON t.modified_by = u.id ");
+    query.append("  WHERE project_id = ");
     query.append(projectID);
-    query.append(" ORDER BY t.active_since DESC ");
-    query.append(" LIMIT 0, 5 ");
 
+    query.append(" UNION ");
+
+    query.append("  SELECT u.id as 'user_id', u.first_name, u.last_name, u.email, t.action, ");
+    query.append("  t.active_since, t.modification_justification ");
+    query.append("  FROM ");
+    query.append(dbName);
+    query.append("_history.ip_project_indicators t ");
+    query.append("  INNER JOIN users u ON t.modified_by = u.id ");
+    query.append("  WHERE project_id = ");
+    query.append(projectID);
+
+    query.append(") temp ");
+    query.append("  GROUP BY email, action, modification_justification, ");
+    // This line group the results that have the value of active_since in a range of +/- 2 seconds
+    query.append(" UNIX_TIMESTAMP(active_since) DIV 2 ");
+    query.append(" ORDER BY active_since DESC ");
+    query.append(" LIMIT 0, 5 ");
     return this.getData(query.toString());
 
   }
