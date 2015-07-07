b2d4c62ab2c09603c8a8ff5c6e62f2a59d9af620
Sebastian Amariles <sebas932@gmail.com>
2015-07-07 11:04:16 -0500

    Improved some style issues and bilateral projects ajax

--- impactPathways/src/main/webapp/js/planning/projectDescriptionPlanning.js
+++ impactPathways/src/main/webapp/js/planning/projectDescriptionPlanning.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
   // Function to load all core projects with ajax
   function loadInitialCoreProjects() {
     $.ajax({
-        'url': '../../coreProjects.do',
+        'url': '../../bilateralCoFinancingProjects.do',
         beforeSend: function() {
           $coreSelect.empty().append(setOption(-1, "Please select a bilateral project"));
         },
