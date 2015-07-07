9ccd2e1ba181eb90db1c45d6fc9c2963d31042cb
Hector Tobon <h.f.tobon@cgiar.org>
2015-07-07 15:28:43 -0500

    Creating back-end (Managers and Model) for the Deliverable Partners.

---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/model/DeliverablePartner.java
@@ -0,0 +1,90 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.ap.data.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * This class represents a partner that belongs to a deliverable with contact information.
+ * 
+ * @author Héctor Fabio Tobón R.
+ */
+public class DeliverablePartner {
+
+  private int id;
+  private Institution institution;
+  private User user;
+  private String type; // Resp, Other.
+
+  public DeliverablePartner() {
+    super();
+  }
+
+  public DeliverablePartner(int id) {
+    super();
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof DeliverablePartner) {
+      DeliverablePartner o = (DeliverablePartner) obj;
+      return o.id == this.id;
+    }
+    return false;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public Institution getInstitution() {
+    return institution;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  @Override
+  public int hashCode() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setInstitution(Institution institution) {
+    this.institution = institution;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+
+}
