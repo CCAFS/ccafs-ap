8c98b227f04a4672c3ffb7711be7457bf17c9d36
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-07-09 16:12:15 -0500

    Improved migration

--- impactPathways/src/main/resources/database/migrations/V2_1_2_20150709_1426__update_ip_project_contributions_overview.sql
+++ impactPathways/src/main/resources/database/migrations/V2_1_2_20150709_1426__update_ip_project_contributions_overview.sql
@@ -9,3 +9,10 @@ ALTER TABLE `ip_project_contribution_overviews` CHANGE COLUMN `project_contribut
     ON DELETE CASCADE ON UPDATE CASCADE, 
   ADD INDEX `FK_project_contribution_overviews_ip_element_idx` (`output_id` ASC), 
   DROP INDEX `FK_project_contribution_idx` ;
+
+  
+ALTER TABLE `ip_project_contribution_overviews` CHARACTER SET = utf8 , ADD COLUMN `project_id` BIGINT NOT NULL  AFTER `id` , 
+  ADD CONSTRAINT `FK_project_contribution_overviews_projects`
+  FOREIGN KEY (`project_id` ) REFERENCES `projects` (`id` )
+  ON DELETE NO ACTION ON UPDATE NO ACTION, 
+  ADD INDEX `FK_project_contribution_overviews_projects_idx` (`project_id` ASC) ;
