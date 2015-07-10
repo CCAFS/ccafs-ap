ffb34629cb9c15a5f0838d8c62b91c1a7aa24c63
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-07-10 11:00:11 -0500

    Added output overview class

---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/model/OutputOverview.java
@@ -0,0 +1,55 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.ap.data.model;
+
+
+/**
+ * @author Hernán David Carvajal B. - CIAT/CCAFS
+ */
+
+public class OutputOverview {
+
+  private int id;
+  private String expectedAnnualContribution;
+  private String socialInclusionDimmension;
+
+  public OutputOverview(int id) {
+    this.id = id;
+  }
+
+  public String getExpectedAnnualContribution() {
+    return expectedAnnualContribution;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getSocialInclusionDimmension() {
+    return socialInclusionDimmension;
+  }
+
+  public void setExpectedAnnualContribution(String expectedAnnualContribution) {
+    this.expectedAnnualContribution = expectedAnnualContribution;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setSocialInclusionDimmension(String socialInclusionDimmension) {
+    this.socialInclusionDimmension = socialInclusionDimmension;
+  }
+}
