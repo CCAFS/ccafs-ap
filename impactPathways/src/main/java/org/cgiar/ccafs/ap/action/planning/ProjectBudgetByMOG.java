e76e89446921b4dec9d27ae49563e15b3e8aac9c
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-07-17 16:28:28 -0500

    Working on budget by MOGs

---
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/action/planning/ProjectBudgetByMOG.java
@@ -0,0 +1,111 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.ap.action.planning;
+
+import org.cgiar.ccafs.ap.action.BaseAction;
+import org.cgiar.ccafs.ap.config.APConstants;
+import org.cgiar.ccafs.ap.data.manager.HistoryManager;
+import org.cgiar.ccafs.ap.data.manager.IPElementManager;
+import org.cgiar.ccafs.ap.data.manager.ProjectManager;
+import org.cgiar.ccafs.ap.data.model.IPElement;
+import org.cgiar.ccafs.ap.data.model.Project;
+import org.cgiar.ccafs.utils.APConfig;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.google.inject.Inject;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author Hernán David Carvajal B. - CIAT/CCAFS
+ */
+
+public class ProjectBudgetByMOG extends BaseAction {
+
+  private static final long serialVersionUID = 4022245671803576328L;
+  private static Logger LOG = LoggerFactory.getLogger(ProjectBudgetByMOG.class);
+
+  // Managers
+  private ProjectManager projectManager;
+  private IPElementManager ipElementManager;
+  private HistoryManager historyManager;
+
+  // Model
+  private Project project;
+  private int projectID;
+
+  @Inject
+  public ProjectBudgetByMOG(APConfig config, ProjectManager projectManager, IPElementManager ipElementManager,
+    HistoryManager historyManager) {
+    super(config);
+    this.projectManager = projectManager;
+    this.ipElementManager = ipElementManager;
+    this.historyManager = historyManager;
+  }
+
+  public int getCurrentPlanningYear() {
+    return config.getPlanningCurrentYear();
+  }
+
+  public int getMidOutcomeYear() {
+    return config.getMidOutcomeYear();
+  }
+
+  public int getMOGIndex(IPElement mog) {
+    int index = 0;
+    List<IPElement> allMOGs = ipElementManager.getIPElements(mog.getProgram(), mog.getType());
+
+    for (int i = 0; i < allMOGs.size(); i++) {
+      if (allMOGs.get(i).getId() == mog.getId()) {
+        return (i + 1);
+      }
+    }
+
+    return index;
+  }
+
+  public Project getProject() {
+    return project;
+  }
+
+  public int getProjectID() {
+    return projectID;
+  }
+
+  @Override
+  public void prepare() throws Exception {
+    projectID = Integer.parseInt(StringUtils.trim(this.getRequest().getParameter(APConstants.PROJECT_REQUEST_ID)));
+
+    // Getting the project information
+    project = projectManager.getProject(projectID);
+    project.setOutputs(ipElementManager.getProjectOutputs(projectID));
+
+    // Remove the outputs duplicated
+    Set<IPElement> outputsTemp = new HashSet<>(project.getOutputs());
+    project.getOutputs().clear();
+    project.getOutputs().addAll(outputsTemp);
+
+    this.setHistory(historyManager.getProjectOutputsHistory(projectID));
+  }
+
+  public void setProjectID(int projectID) {
+    this.projectID = projectID;
+  }
+}
