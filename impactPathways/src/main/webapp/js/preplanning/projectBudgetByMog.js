dcbba9ef62c1242f3850250126d11d347169124d
Sebastian Amariles <sebas932@gmail.com>
2015-07-22 16:20:38 -0500

    Working on project budget by MOGs

--- impactPathways/src/main/webapp/js/preplanning/projectBudgetByMog.js
+++ impactPathways/src/main/webapp/js/preplanning/projectBudgetByMog.js
@@ -6,13 +6,17 @@ $(document).ready(init);
 function init() {
   // Setting vars
   $percentageInputs = $("input.percentage");
+  $budgetInputs = $("input.budgetInput");
+  $genderBudgetInputs = $("input.genderBudgetInput");
+
+  budgetByYear = new BudgetRemaining('#budgetByYear');
+  genderBudgetByYear = new BudgetRemaining('#genderBudgetByYear');
 
   // Attach events
   attachEvents();
 
   // Active initial currency format to all inputs
-  $percentageInputs.attr("autocomplete", "off");
-  $percentageInputs.trigger("focusout");
+  $percentageInputs.attr("autocomplete", "off").trigger("focusout");
 
   // Validate justification and information
   validateEvent('[name=save], [name=next]', [
@@ -24,11 +28,18 @@ function init() {
 }
 
 function attachEvents() {
-
   $percentageInputs.on("keydown", isNumber).on("focusout", setPercentage).on("focus", removePercentage).on("click",
       function() {
         $(this).select();
       });
+
+  $budgetInputs.on("keyup", function(e) {
+    checkPercentages($(e.target), $budgetInputs, budgetByYear);
+  });
+
+  $genderBudgetInputs.on("keyup", function(e) {
+    checkPercentages($(e.target), $genderBudgetInputs, genderBudgetByYear);
+  });
 
   $("form").submit(function(event) {
     $percentageInputs.each(function() {
@@ -37,7 +48,31 @@ function attachEvents() {
     });
     return;
   });
+}
 
+function BudgetRemaining(budget) {
+  this.getValue = $(budget).find('input').val();
+  this.setValue = function(value) {
+    $(budget).find('span').text(setCurrencyFormat(value));
+    $(budget).find('input').val(value);
+  };
+  this.calculateRemain = function(percentage) {
+    var result = (this.getValue / 100) * percentage;
+    this.setValue(this.getValue - result);
+  };
+}
+
+function checkPercentages(inputTarget,inputList,remainBudget) {
+  var result = 0;
+  $(inputList).removeClass('fieldError');
+  $(inputList).each(function(i,input) {
+    result += parseFloat(removePercentageFormat($(input).val() || 0));
+  });
+  if(result > 100) {
+    $(inputTarget).addClass('fieldError');
+  } else {
+    remainBudget.calculateRemain(result);
+  }
 }
 
 function setPercentage(event) {
