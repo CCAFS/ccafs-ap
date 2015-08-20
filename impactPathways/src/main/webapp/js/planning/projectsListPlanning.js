05abf87495ab922a2a09649e4d5e324b115a1a53
carlosm <carlos.martinez@cgiar.org>
2015-08-20 15:13:14 -0500

    Sort ID bug in projectsList

--- impactPathways/src/main/webapp/js/planning/projectsListPlanning.js
+++ impactPathways/src/main/webapp/js/planning/projectsListPlanning.js
@@ -1,3 +1,52 @@
+jQuery.fn.dataTableExt.aTypes.unshift(function(sData) {
+  sData = typeof sData.replace == 'function' ? sData.replace(/<[\s\S]*?>/g, "") : sData;
+  sData = $.trim(sData);
+
+  var sValidFirstChars = "0123456789-";
+  var sValidChars = "0123456789.";
+  var Char;
+  var bDecimal = false;
+
+  /* Check for a valid first char (no period and allow negatives) */
+  Char = sData.charAt(0);
+  if(sValidFirstChars.indexOf(Char) == -1) {
+    return null;
+  }
+
+  /* Check all the other characters are valid */
+  for(var i = 1; i < sData.length; i++) {
+    Char = sData.charAt(i);
+    if(sValidChars.indexOf(Char) == -1) {
+      return null;
+    }
+
+    /* Only allowed one decimal place... */
+    if(Char == ".") {
+      if(bDecimal) {
+        return null;
+      }
+      bDecimal = true;
+    }
+  }
+
+  return 'num-html';
+});
+
+jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+    "num-html-pre": function(a) {
+      var x = String(a).replace(/<[\s\S]*?>/g, "");
+      return parseFloat(x);
+    },
+
+    "num-html-asc": function(a,b) {
+      return((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "num-html-desc": function(a,b) {
+      return((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+});
+
 $(document).ready(function() {
   $('table.projectsList').dataTable({
       "bPaginate": true, // This option enable the table pagination
@@ -14,12 +63,17 @@ $(document).ready(function() {
         }
       },
       aoColumnDefs: [
-        {
-            bSortable: false,
-            aTargets: [
-                -1, -2, -3
-            ]
-        }
+          {
+              bSortable: false,
+              aTargets: [
+                  -1, -2, -3,
+              ]
+          }, {
+              sType: "num-html",
+              aTargets: [
+                0
+              ]
+          }
       ]
   });
   $('table.projectsList').on('draw.dt', function() {
