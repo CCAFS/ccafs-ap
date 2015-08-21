e39a70c7105ec2992ae9c1a9aa5b0b979d1f9d24
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-08-21 09:12:30 -0500

    Improved query to deactive unused budgets.

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLBudgetDAO.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/dao/mysql/MySQLBudgetDAO.java
@@ -401,9 +401,10 @@ public class MySQLBudgetDAO implements BudgetDAO {
     query.append("SET pb.is_active = FALSE ");
     query.append("WHERE pb.institution_id NOT IN ");
     query.append("( SELECT  partner_id FROM project_partners pp ");
-    query.append("  INNER JOIN ");
-    query.append("  WHERE pp.project_id = pb.project_id AND pp.is_active = 1 GROUP BY pp.partner_id, pp.is_active ) ");
-    query.append(" AND pb.project_id = ");
+    query.append("  INNER JOIN institutions i ON pp.partner_id = i.id ");
+    query.append("  WHERE pp.project_id = pb.project_id AND pp.is_active = TRUE AND i.is_ppa = TRUE ");
+    query.append("  GROUP BY pp.partner_id, pp.is_active ");
+    query.append(") AND pb.project_id = ");
     query.append(projectID);
 
     int result = databaseManager.saveData(query.toString(), new Object[] {});
