805c42b184015399ad8f92e8fdd70d8b3beb0784
Hernan Carvajal <carvajal.hernandavid@gmail.com>
2015-08-27 15:34:42 -0500

    Removing budgets of institutions which are not partners or ppa partners.

--- impactPathways/src/main/resources/database/migrations/V3_0_1_20150827_1456__Fixing_projects_details.sql
+++ impactPathways/src/main/resources/database/migrations/V3_0_1_20150827_1456__Fixing_projects_details.sql
@@ -6,3 +6,13 @@ ALTER TABLE `project_partners` DROP COLUMN `is_leader` ;
 
 -- Deleting a record of a project that have 2 leaders
 DELETE FROM `project_partner_persons` WHERE `id` = 805;
+
+-- Removing all the budgets that belong to institution which are not partners or ppa partners of the project.
+UPDATE project_budgets pb 
+SET pb.is_active = FALSE 
+WHERE pb.institution_id NOT IN
+( SELECT  partner_id FROM project_partners pp
+  INNER JOIN institutions i ON pp.partner_id = i.id
+  WHERE pp.project_id = pb.project_id AND pp.is_active = TRUE AND i.is_ppa = TRUE
+  GROUP BY pp.partner_id, pp.is_active
+) 
