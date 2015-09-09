ca9ab315c32164fb343ee2f1298503c5c0dbd052
carlosm <carlos.martinez@cgiar.org>
2015-09-09 10:03:41 -0500

    ProjectsList-Home sorting

--- impactPathways/src/main/webapp/js/planning/projectsListPlanning.js
+++ impactPathways/src/main/webapp/js/planning/projectsListPlanning.js
@@ -31,6 +31,33 @@ $(document).ready(function() {
     $("a.removeProject").on("click", removeProject);
   });
 
+  $('table.projectsHome').dataTable({
+      "bPaginate": true, // This option enable the table pagination
+      "bLengthChange": true, // This option disables the select table size option
+      "bFilter": true, // This option enable the search
+      "bSort": true, // this option enable the sort of contents by columns
+      "bAutoWidth": false, // This option enables the auto adjust columns width
+      "iDisplayLength": 5, // Number of rows to show on the table
+      "fnDrawCallback": function() {
+        // This function locates the add activity button at left to the filter box
+        var table = $(this).parent().find("table");
+        if($(table).attr("id") == "currentActivities") {
+          $("#currentActivities_filter").prepend($("#addActivity"));
+        }
+      },
+      aoColumnDefs: [
+        {
+            sType: "natural",
+            aTargets: [
+              0
+            ]
+        }
+      ]
+  });
+  $('table.projectsHome').on('draw.dt', function() {
+    $("a.removeProject").on("click", removeProject);
+  });
+
   $("#submitForm").on("submit", function(evt) {
     if(confirm($("#beforeSubmitMessage").val())) {
       return true;
