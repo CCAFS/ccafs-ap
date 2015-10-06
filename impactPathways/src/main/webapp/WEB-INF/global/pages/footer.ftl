031ac97e6e953af723490f5d173c73512e63ec6e
Sebastian Amariles <sebas932@gmail.com>
2015-10-06 08:50:33 -0500

    Improving Tawk.to plugin

--- impactPathways/src/main/webapp/WEB-INF/global/pages/footer.ftl
+++ impactPathways/src/main/webapp/WEB-INF/global/pages/footer.ftl
@@ -1,19 +1,6 @@
  [#ftl]
  	</div> <!-- end container -->   
       <footer>
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/56128da200d3af75029e5645/default';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-          })();
-        </script>
-        <!--End of Tawk.to Script-->
         <div class="container">
           <div id="footerContainer" class="little">
             <div id="reportIssue">
--- impactPathways/src/main/webapp/js/global/global.js
+++ impactPathways/src/main/webapp/js/global/global.js
@@ -3,6 +3,7 @@ var formBefore;
 var justificationLimitWords = 100;
 var errorMessages = [];
 var forceChange = false;
+var Tawk_API, Tawk_LoadStart;
 var notyDefaultOptions = {
     text: '',
     layout: 'bottomRight',
@@ -108,6 +109,29 @@ $(document).ready(function() {
   }
 
   $("textarea[id!='justification']").autoGrow();
+
+  // Adding Tawk.to plugin
+  Tawk_API = Tawk_API || {};
+  Tawk_LoadStart = new Date();
+
+  Tawk_API.onLoad = function() {
+    Tawk_API.visitor = {
+      name: $('#userInfo p.email').text() || 'Unknow user',
+    };
+    Tawk_API.setAttributes({
+      'userName': $('#userInfo p.email').text() || 'Unknow user'
+    }, function(error) {
+      console.log(error);
+    });
+  };
+  (function() {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/56128da200d3af75029e5645/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
 
   // Generating hash from form information
   setFormHash();
