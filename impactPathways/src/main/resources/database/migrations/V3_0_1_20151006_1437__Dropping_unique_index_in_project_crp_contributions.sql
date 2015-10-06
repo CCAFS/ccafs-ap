696722b8b1e79b747394d9002b9f889903d0d4f6
Hector Tobon <h.f.tobon@cgiar.org>
2015-10-06 14:40:09 -0500

    Deleting unique index in project_crp_contributions table.

---
+++ impactPathways/src/main/resources/database/migrations/V3_0_1_20151006_1437__Dropping_unique_index_in_project_crp_contributions.sql
@@ -0,0 +1,5 @@
+----------------------------------------------------------------------------
+--    Drop unique index in table 'project_crp_contributions' 
+----------------------------------------------------------------------------
+ALTER TABLE `project_crp_contributions` 
+DROP INDEX `UK_project_crp_contributions` ;
