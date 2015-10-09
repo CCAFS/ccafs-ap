f549933c317eda3f3d3f0b9295e1cbf5bcb5957f
cgarcia9106 <c.d.garcia@cgiar.org>
2015-10-09 11:04:38 -0500

    Removing delete index to bd history

--- impactPathways/src/main/resources/database/migrations/V3_0_1_20151009_0860__Removing_unique_index_project_partner.sql
+++ impactPathways/src/main/resources/database/migrations/V3_0_1_20151009_0860__Removing_unique_index_project_partner.sql
@@ -3,7 +3,3 @@
 -----------------------------------------------------------------------
 ALTER TABLE `project_partners` 
 DROP INDEX `UK_project_partners` ;
-
-use ccafspr_ip_history;
-ALTER TABLE `project_partners` 
-DROP INDEX `UK_project_partners` ;
