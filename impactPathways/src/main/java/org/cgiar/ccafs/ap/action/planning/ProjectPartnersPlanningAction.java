84c6a351e5c92ea13ce3a9e8d98263fd67109ef7
Hector Tobon <h.f.tobon@cgiar.org>
2015-10-09 10:54:27 -0500

    Project roles are not needed.

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/action/planning/ProjectPartnersPlanningAction.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/action/planning/ProjectPartnersPlanningAction.java
@@ -467,7 +467,7 @@ public class ProjectPartnersPlanningAction extends BaseAction {
     // Initializing Section Statuses:
     this.initializeProjectSectionStatuses(project, "Planning");
 
-    // set History.
+    // Set History.
     super.setHistory(historyManager.getProjectPartnersHistory(project.getId()));
 
   }
@@ -494,9 +494,6 @@ public class ProjectPartnersPlanningAction extends BaseAction {
 
       projectPartnerManager.saveProjectPartners(project, project.getProjectPartners(), this.getCurrentUser(),
         this.getJustification());
-
-      // TODO Figure out how is being used the project_roles table in the system.
-      projectRoleManager.saveProjectRoles(project, this.getCurrentUser(), this.getJustification());
 
       // Check if the project leader has changed and send the corresponding emails
       PartnerPerson previousLeader = previousProject.getLeaderPerson();
