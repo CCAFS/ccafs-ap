92b042c1e616ed3715612d14c7a9c337e9db5430
cgarcia9106 <c.d.garcia@cgiar.org>
2015-10-09 11:23:44 -0500

    Correction about removing index on histroy bd

--- impactPathways/src/main/resources/database/migrations/V3_0_1_20151009_0860__Removing_unique_index_project_partner.sql
+++ impactPathways/src/main/resources/database/migrations/V3_0_1_20151009_0860__Removing_unique_index_project_partner.sql
@@ -3,3 +3,22 @@
 -----------------------------------------------------------------------
 ALTER TABLE `project_partners` 
 DROP INDEX `UK_project_partners` ;
+
+
+ALTER TABLE $[database]_history.project_partners 
+DROP INDEX `UK_project_partners` ;
+
+
+ALTER TABLE `project_partner_contributions` 
+DROP INDEX `UK_project_partner_contributions` ;
+
+
+ALTER TABLE $[database]_history.project_partner_contributions 
+DROP INDEX `UK_project_partner_contributions` ;
+
+ALTER TABLE `project_partner_persons` 
+DROP INDEX `UK_project_partner_persons` ;
+
+
+ALTER TABLE $[database]_history.project_partner_persons 
+DROP INDEX `UK_project_partner_persons` ;
