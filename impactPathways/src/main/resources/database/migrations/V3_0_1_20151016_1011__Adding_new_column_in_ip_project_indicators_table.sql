0b79c52a6cf4a706cdf32d91e422c078441256e1
Hector Tobon <h.f.tobon@cgiar.org>
2015-10-16 10:46:37 -0500

    empty commit.

--- impactPathways/src/main/resources/database/migrations/V3_0_1_20151016_1011__Adding_new_column_in_ip_project_indicators_table.sql
+++ impactPathways/src/main/resources/database/migrations/V3_0_1_20151016_1011__Adding_new_column_in_ip_project_indicators_table.sql
@@ -1,5 +1,10 @@
-----------------------------------------------------------------------------
---    Drop unique index in table 'project_crp_contributions' 
-----------------------------------------------------------------------------
-ALTER TABLE `project_budgets` 
-DROP INDEX `UK_project_budgets` ;
+----------------------------------------------------------------------------------------------------------
+-- Adding a new column in the table ip_project_indicators to get Gender contributions towards indicators.
+----------------------------------------------------------------------------------------------------------
+
+-- Creating column in database
+ALTER TABLE `ip_project_indicators` 
+CHANGE COLUMN `target` `target` TEXT NULL DEFAULT NULL COMMENT '' AFTER `id`,
+ADD COLUMN `gender` TEXT NULL COMMENT '' AFTER `description`;
+
+
