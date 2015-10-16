b1af5040dd43bdf8fe88ac4d6b68cc2a87d7ca00
Hector Tobon <h.f.tobon@cgiar.org>
2015-10-16 11:56:06 -0500

    Adding a new column into the ip_project_indicators table whithout deleting any record in the hstory. Triggers were updated accordingly.

--- impactPathways/src/main/resources/database/migrations/V3_0_1_20151016_1011__Adding_new_column_in_ip_project_indicators_table.sql
+++ impactPathways/src/main/resources/database/migrations/V3_0_1_20151016_1011__Adding_new_column_in_ip_project_indicators_table.sql
@@ -7,4 +7,7 @@ ALTER TABLE `ip_project_indicators`
 CHANGE COLUMN `target` `target` TEXT NULL DEFAULT NULL COMMENT '' AFTER `id`,
 ADD COLUMN `gender` TEXT NULL COMMENT '' AFTER `description`;
 
-
+-- Creating column in history
+ALTER TABLE $[database]_history.`ip_project_indicators` 
+CHANGE COLUMN `target` `target` TEXT NULL DEFAULT NULL COMMENT '' AFTER `record_id`,
+ADD COLUMN `gender` TEXT NULL COMMENT '' AFTER `description`;
