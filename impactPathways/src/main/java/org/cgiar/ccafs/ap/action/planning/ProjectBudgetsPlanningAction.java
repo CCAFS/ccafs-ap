4086af3954f69ffc2d097c9787b578be7e5d3a35
cgarcia9106 <c.d.garcia@cgiar.org>
2015-10-19 08:53:17 -0500

    Adding update

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/action/planning/ProjectBudgetsPlanningAction.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/action/planning/ProjectBudgetsPlanningAction.java
@@ -375,10 +375,13 @@ public class ProjectBudgetsPlanningAction extends BaseAction {
         }
       }
 
+
       // Adjust the type of all projects according to their links with other projects.
       projectManager.updateProjectTypes();
       budgetManager.deleteBudgetsWithNoLinkToInstitutions(projectID, this.getCurrentPlanningYear());
+      if (project.getLinkedProjects().isEmpty()) {
 
+      }
       if (!success) {
         this.addActionError(this.getText("saving.problem"));
         return BaseAction.INPUT;
