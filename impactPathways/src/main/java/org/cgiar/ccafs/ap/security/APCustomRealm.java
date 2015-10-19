4976f6dc7558dd9c670b9a88212a1d9f6d9d8c32
Hector Tobon <h.f.tobon@cgiar.org>
2015-10-19 15:54:23 -0500

    Improving login performance with Shiro.

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/security/APCustomRealm.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/security/APCustomRealm.java
@@ -31,6 +31,7 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.name.Named;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -44,6 +45,7 @@ import org.apache.shiro.authc.credential.CredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -61,7 +63,7 @@ public class APCustomRealm extends AuthorizingRealm {
 
   // Variables
   final AllowAllCredentialsMatcher credentialsMatcher = new AllowAllCredentialsMatcher();
-  private SimpleAuthorizationInfo authorizationInfo;
+  // private SimpleAuthorizationInfo authorizationInfo;
   private int userID;
 
   // Managers
@@ -93,7 +95,7 @@ public class APCustomRealm extends AuthorizingRealm {
   @Override
   public void clearCachedAuthorizationInfo(PrincipalCollection principals) {
     super.clearCachedAuthorizationInfo(principals);
-    authorizationInfo = null;
+    // authorizationInfo = null;
   }
 
   @Override
@@ -139,71 +141,75 @@ public class APCustomRealm extends AuthorizingRealm {
 
   @Override
   protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-    // if (authorizationInfo == null) {
-    // if ((Integer) principals.getPrimaryPrincipal() != userID) {
-    authorizationInfo = new SimpleAuthorizationInfo();
+    Session session = SecurityUtils.getSubject().getSession();
+    if (session.getAttribute("auth_info") == null) {
+      // if ((Integer) principals.getPrimaryPrincipal() != userID) {
+      SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+      session.setAttribute("auth_info", authorizationInfo);
 
-    userID = (Integer) principals.getPrimaryPrincipal();
-    List<UserRole> roles = userRoleManager.getUserRolesByUserID(String.valueOf(userID));
-    Map<String, UserRole> projectRoles = new HashMap<>();
+      userID = (Integer) principals.getPrimaryPrincipal();
+      List<UserRole> roles = userRoleManager.getUserRolesByUserID(String.valueOf(userID));
+      Map<String, UserRole> projectRoles = new HashMap<>();
 
-    // Get the roles general to the platform
-    for (UserRole role : roles) {
-      authorizationInfo.addRole(role.getAcronym());
+      // Get the roles general to the platform
+      for (UserRole role : roles) {
+        authorizationInfo.addRole(role.getAcronym());
 
-      switch (role.getId()) {
-        case APConstants.ROLE_ADMIN:
-          for (String permission : role.getPermissions()) {
-            authorizationInfo.addStringPermission(permission);
+        switch (role.getId()) {
+          case APConstants.ROLE_ADMIN:
+            for (String permission : role.getPermissions()) {
+              authorizationInfo.addStringPermission(permission);
+            }
+            break;
+
+          case APConstants.ROLE_MANAGEMENT_LIAISON:
+            projectRoles.putAll(userRoleManager.getManagementLiaisonProjects(userID));
+            break;
+
+          case APConstants.ROLE_PROJECT_LEADER:
+          case APConstants.ROLE_PROJECT_COORDINATOR:
+            projectRoles.putAll(userRoleManager.getProjectLeaderProjects(userID));
+            break;
+
+          case APConstants.ROLE_CONTACT_POINT:
+            projectRoles.putAll(userRoleManager.getContactPointProjects(userID));
+            break;
+        }
+      }
+
+      // Converting those general roles into specific for the projects where they are able to edit.
+      for (Map.Entry<String, UserRole> entry : projectRoles.entrySet()) {
+        String projectID = entry.getKey();
+        UserRole role = entry.getValue();
+
+        for (String permission : role.getPermissions()) {
+          // Add the project identifier to the permission only if the permission is not at project level.
+          // The following permission will be ignored: planning:projects:5:description:update
+          // if (!permission.matches("((?:project:[\0-9]{1,10}:)")) {
+          if (permission.contains(":projects:")) {
+            permission = permission.replace("projects:", "projects:" + projectID + ":");
           }
-          break;
-
-        case APConstants.ROLE_MANAGEMENT_LIAISON:
-          projectRoles.putAll(userRoleManager.getManagementLiaisonProjects(userID));
-          break;
-
-        case APConstants.ROLE_PROJECT_LEADER:
-        case APConstants.ROLE_PROJECT_COORDINATOR:
-          projectRoles.putAll(userRoleManager.getProjectLeaderProjects(userID));
-          break;
-
-        case APConstants.ROLE_CONTACT_POINT:
-          projectRoles.putAll(userRoleManager.getContactPointProjects(userID));
-          break;
-      }
-    }
-
-    // Converting those general roles into specific for the projects where they are able to edit.
-    for (Map.Entry<String, UserRole> entry : projectRoles.entrySet()) {
-      String projectID = entry.getKey();
-      UserRole role = entry.getValue();
-
-      for (String permission : role.getPermissions()) {
-        // Add the project identifier to the permission only if the permission is not at project level.
-        // The following permission will be ignored: planning:projects:5:description:update
-        // if (!permission.matches("((?:project:[\0-9]{1,10}:)")) {
-        if (permission.contains(":projects:")) {
-          permission = permission.replace("projects:", "projects:" + projectID + ":");
+          authorizationInfo.addStringPermission(permission);
+          // }
         }
-        authorizationInfo.addStringPermission(permission);
-        // }
       }
-    }
 
-    // Getting the specific roles based on the table project_roles.
-    List<ProjectUserRole> projectSpecificUserRoles = projectSpecificUserRoleManager.getProjectSpecificUserRoles(userID);
-    // Adding the specific project roles to the user.
-    for (ProjectUserRole projectUserRole : projectSpecificUserRoles) {
-      for (String permission : projectUserRole.getUserRole().getPermissions()) {
-        if (permission.contains(":projects:")) {
-          permission = permission.replace("projects:", "projects:" + projectUserRole.getProjectID() + ":");
+      // Getting the specific roles based on the table project_roles.
+      List<ProjectUserRole> projectSpecificUserRoles =
+        projectSpecificUserRoleManager.getProjectSpecificUserRoles(userID);
+      // Adding the specific project roles to the user.
+      for (ProjectUserRole projectUserRole : projectSpecificUserRoles) {
+        for (String permission : projectUserRole.getUserRole().getPermissions()) {
+          if (permission.contains(":projects:")) {
+            permission = permission.replace("projects:", "projects:" + projectUserRole.getProjectID() + ":");
+          }
+          authorizationInfo.addStringPermission(permission);
         }
-        authorizationInfo.addStringPermission(permission);
       }
+      return authorizationInfo;
     }
-    // }
+    return (AuthorizationInfo) session.getAttribute("auth_info");
 
-    return authorizationInfo;
   }
 
   @Override
