c8148cb2d8478dfa870116b33eb870f88549296e
cgarcia9106 <c.d.garcia@cgiar.org>
2015-12-11 14:51:29 -0500

    ok

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/config/FlywayContextListener.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/config/FlywayContextListener.java
@@ -74,6 +74,7 @@ public class FlywayContextListener implements ServletContextListener {
     // flyway.setValidateOnMigrate(false);
     flyway.setOutOfOrder(true);
     flyway.migrate();
+    System.out.println("Finaliza los listenrs");
 
   }
 
