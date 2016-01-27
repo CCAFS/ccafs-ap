b1b0ae73cd0fa7b6b43beb810b3c880787b76f0d
Hector Tobon <h.f.tobon@cgiar.org>
2016-01-27 11:43:09 -0500

    re-organizing permissions for submitProject and deleteProject

---
+++ impactPathways/src/main/resources/database/migrations/V4_0_1_20160127_1128__Updating_permissions_for_submit_and_delete_projects.sql
@@ -0,0 +1,24 @@
+-- Deleting permissions refered to projects list.
+DELETE rp FROM role_permissions rp 
+INNER JOIN permissions p ON rp.permission_id = p.id
+WHERE p.permission like "%submit%";
+
+-- Updating permissions
+DELETE FROM `permissions` WHERE `id`='6';
+UPDATE `permissions` SET `permission`='planning:projects:manage:submitProject', `description`='Can use the \"Submit project\" button in any section in planning round.' WHERE `id`='97';
+UPDATE `permissions` SET `permission`='reporting:projects:manage:submitProject', `description`='Can use the \"Submit project\" button in any section in reporting round.' WHERE `id`='102';
+UPDATE `permissions` SET `permission`='planning:projects:manage:deleteProject', `description`='Can use the \"Delete project\" button in any section in planning round.' WHERE `id`='7';
+UPDATE `permissions` SET `permission`='reporting:projects:manage:deleteProject', `description`='Can use the \"Delete project\" button in any section in reporting round.' WHERE `id`='101';
+
+
+-- Assingning roles.
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'ML'), permission_id = (SELECT id FROM permissions WHERE permission = 'planning:projects:manage:submitProject');
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'CP'), permission_id = (SELECT id FROM permissions WHERE permission = 'planning:projects:manage:submitProject');
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'PL'), permission_id = (SELECT id FROM permissions WHERE permission = 'planning:projects:manage:submitProject');
+
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'ML'), permission_id = (SELECT id FROM permissions WHERE permission = 'reporting:projects:manage:submitProject');
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'CP'), permission_id = (SELECT id FROM permissions WHERE permission = 'reporting:projects:manage:submitProject');
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'PL'), permission_id = (SELECT id FROM permissions WHERE permission = 'reporting:projects:manage:submitProject');
+
+INSERT INTO role_permissions SET role_id = (SELECT id FROM roles WHERE acronym = 'CP'), permission_id = (SELECT id FROM permissions WHERE permission = 'planning:projects:manage:deleteProject');
+
