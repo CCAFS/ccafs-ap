90c29c620cb914ab14d4d170f283bc69c50a23d3
cgarcia9106 <c.d.garcia@cgiar.org>
2016-05-02 15:04:40 -0500

    [PE]: Evualiton List

--- impactPathways/src/main/java/org/cgiar/ccafs/ap/data/model/ProjectEvaluation.java
+++ impactPathways/src/main/java/org/cgiar/ccafs/ap/data/model/ProjectEvaluation.java
@@ -109,11 +109,18 @@ public class ProjectEvaluation implements java.io.Serializable {
       return false;
     }
     ProjectEvaluation other = (ProjectEvaluation) obj;
-    if (id == null) {
-      if (other.id != null) {
+    if (programId == null) {
+      if (other.programId != null) {
         return false;
       }
-    } else if (!id.equals(other.id)) {
+    } else if (!programId.equals(other.programId)) {
+      return false;
+    }
+    if (userId == null) {
+      if (other.userId != null) {
+        return false;
+      }
+    } else if (!userId.equals(other.userId)) {
       return false;
     }
     return true;
@@ -222,7 +229,8 @@ public class ProjectEvaluation implements java.io.Serializable {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((programId == null) ? 0 : programId.hashCode());
+    result = prime * result + ((userId == null) ? 0 : userId.hashCode());
     return result;
   }
 
